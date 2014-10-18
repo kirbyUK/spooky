909d20f469c7fc7dfdef9a55514936f19314b398
Alex Kerr <kirbyman62@gmail.com>
2014-10-18 02:08:01 +0100

    Moved the drawing to it's own function

--- spooky.pl
+++ spooky.pl
@@ -8,32 +8,41 @@ use Interface;
 #Seed the RNG:
 srand time;
 
+# Make the character:
 my $char = Character->new;
 $char->generate_new_stats;
+
+# Initialise the interface:
 my $interface = Interface->new;
-my $str = $char->stats->{strength};
-$interface->draw
-(
-	{
-		stats =>
-		[
-			{
-				x => 0,
-				y => 0,
-				text => "Strength: ${\$char->stats->{strength}}",
-			},
-			{
-				x => 0,
-				y => 1,
-				text => "Defence:  ${\$char->stats->{defence}}",
-			},
-			{
-				x => 0,
-				y => 2,
-				text => "Speed:    ${\$char->stats->{speed}}",
-			},
-		],
-	},
-);
+&draw;
 Curses::getch;
 $interface->close;
+
+# Draws all the stuff needed:
+sub draw
+{
+	$interface->draw
+	(
+		{
+			# The stats window:
+			stats =>
+			[
+				{
+					x => 0,
+					y => 0,
+					text => "Strength: ${\$char->stats->{strength}}",
+				},
+				{
+					x => 0,
+					y => 1,
+					text => "Defence:  ${\$char->stats->{defence}}",
+				},
+				{
+					x => 0,
+					y => 2,
+					text => "Speed:    ${\$char->stats->{speed}}",
+				},
+			],
+		},
+	);
+}
