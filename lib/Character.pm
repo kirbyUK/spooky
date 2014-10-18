43c45c7217ac620e89b4a5b6279f889908b3ed5d
Alex Kerr <kirbyman62@gmail.com>
2014-10-18 02:02:16 +0100

    Added Character class

---
+++ lib/Character.pm
@@ -0,0 +1,71 @@
+package Character;
+
+sub new
+{
+	my $class = shift;
+	my $properties =
+	{
+		# The position (relative to the main window, not stdscr):
+		position =>
+		{
+			x => 100,
+			y => 100,
+		},
+
+		# The amount of candy the player has:
+		candy => 5,
+	};
+	bless $properties, $class;
+}
+
+# Modifies the player's position:
+sub move
+{
+	my $self = shift;
+	my $vector = shift;
+	$self->{position}->{x} += $vector->{x};
+	$self->{position}->{y} += $vector->{y};
+}
+
+# Returns the player's position:
+sub position
+{
+	my $self = shift;
+	$self->{position};
+}
+
+# Adds the given amount of candy:
+sub add_candy
+{
+	my $self = shift;
+	my $value = shift;
+	$self->{candy} += $value;
+}
+
+# Gets candy:
+sub candy
+{
+	my $self = shift;
+	$self->{candy};
+}
+
+# Generates a new stat spread:
+sub generate_new_stats
+{
+	my $self = shift;
+	$self->{stats} =
+	{
+		strength => 10,
+		defence => 10,
+		speed => 10,
+	};
+}
+
+# Gets the player's stats:
+sub stats
+{
+	my $self = shift;
+	$self->{stats};
+}
+
+1;
