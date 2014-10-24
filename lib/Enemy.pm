94aea12bb11eb5e7fd881651eacd2e2dcdbaea67
Alex Kerr <kirbyman62@gmail.com>
2014-10-24 13:26:17 +0100

    More debug stuff

--- lib/Enemy.pm
+++ lib/Enemy.pm
@@ -15,9 +15,6 @@ sub move
 	my $self = shift;
 	my $player = shift;
 
-	open my $file, '>', "debug";
-	print $file Dumper($player);
-
 	# Homes in on the player:
 	$self->position->{x} += ($player->position->{x} <=> $self->position->{x});
 	$self->position->{y} += ($player->position->{y} <=> $self->position->{y});
